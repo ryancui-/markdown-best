f3f4d8dc1e7d1f647f5111d8eaf626e8d7a47f58
ryancui- <cuizhaowei92@gmail.com>
2017-07-22 11:35:40 +0800

    [dev]: a flexible panels with resizing - move hightlight to lib/

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import 'github-markdown-css/github-markdown.css';
 import MarkdownIt from 'markdown-it';
-import './highlight.pack';
-import './highlight.css';
-import './index.css';
+import './lib/highlight.pack';
+import './lib/highlight.css';
+import './index.less';
+
+(() => {
+
+  const $body = document.getElementsByTagName('body')[0];
+  const $grid = document.getElementsByClassName('grid-wrapper')[0];
+  const $split = document.getElementsByClassName('grid-split')[0];
+
+  // handle resize event
+  var handleMousemove = (event) => {
+    event.preventDefault();
+
+    const leftSize = (event.screenX - 7.5) / document.body.clientWidth * 100;
+    const rightSize = 100 - leftSize;
+
+    $grid.style.gridTemplateColumns = `${leftSize}% 15px ${rightSize}%`;
+  };
+
+  $split.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    $body.addEventListener('mousemove', handleMousemove);
+  });
+
+  $body.addEventListener('mouseup', (e) => {
+    e.preventDefault();
+    $body.removeEventListener('mousemove', handleMousemove);
+  });
+
+})();
