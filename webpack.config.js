662a3f24de8dcdce50f05433e99b9b4fe938edb4
ryancui- <cuizhaowei92@gmail.com>
2017-07-26 21:21:15 +0800

    [chore]: add cross-env dependency and a github-page version - extract styles.css to let the page not flipping

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,58 @@
  */
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'docs')
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [{
       test: /\.less$/,
-      use: ['style-loader', 'css-loader', 'less-loader']
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'less-loader']
+      })
     }, {
       test: /\.css$/,
-      use: ['style-loader', 'css-loader']
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
     }, {
       test: /\.js$/,
       use: ['babel-loader'],
       exclude: /node_modules/
     }]
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'index.html'
-  })],
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
   devServer: {
+    historyApiFallback: true,
     noInfo: true
   },
-  devtool: 'eval-source-map'
+  devtool: '#eval-source-map'
 };
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'github-page') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]);
+}
+
+if (process.env.NODE_ENV === 'github-page') {
+  module.exports.output.path = path.resolve(__dirname, 'docs');
+}
