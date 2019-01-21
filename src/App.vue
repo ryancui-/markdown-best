3212af55b262a0cc6edb68918e8b33aeb71d0f52
ryancui- <cuizhaowei92@gmail.com>
2019-01-21 17:31:37 +0800

    init: init the markdown-editor project with Vue.js@2.x

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>123123</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+html, body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
